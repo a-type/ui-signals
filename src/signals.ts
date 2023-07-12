081de38741febba2f110cf157be85a5db96b0eb6
Grant Forrest <gaforres@gmail.com>
2023-07-12 14:12:26 -0400

    fix signals usages

--- src/signals.ts
+++ src/signals.ts
@@ -11,14 +11,17 @@ export function atom<T>(initial: T, name?: string) {
 
 export function computed<T, Diff>(
   compute: (previousValue: T, lastComputedEpoch: number) => T,
-  options?: ComputedOptions<T, Diff> & { name?: string },
+  options: ComputedOptions<T, Diff> & { name?: string } = {},
 ) {
   return baseComputed(options.name ?? 'computed', compute, options);
 }
 
 export function react(
   callback: (lastChanged: number) => void,
-  options?: { scheduleEffect?: (execute: () => void) => void; name?: string },
+  options: {
+    scheduleEffect?: (execute: () => void) => void;
+    name?: string;
+  } = {},
 ) {
   return baseReact(options.name ?? 'react', callback, options);
 }
